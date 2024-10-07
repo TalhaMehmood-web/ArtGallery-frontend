8a0861dc50897e4c81941ac9649c10a6c921d870
TalhaMehmood-web <talhamehmood991@gmail.com>
2024-10-08 02:55:52 +0500

    commit

--- src/components/miscellaneous/client/ProfileItem.jsx
+++ src/components/miscellaneous/client/ProfileItem.jsx
@@ -1,6 +1,6 @@
 const ProfileItem = ({ header, content }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-1 p-1 text-white border-yellow-500 rounded-md bg-yellow-500/80 ">
+    <div className="flex flex-col items-center justify-center gap-1 p-1 text-white border border-green-600 rounded-md bg-green-600/50 ">
       <p className=" md:text-lg semibold">{header}</p>
       <p className="text-4xl font-extrabold text-center">{content}</p>
     </div>
