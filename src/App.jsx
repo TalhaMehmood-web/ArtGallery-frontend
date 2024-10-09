76bb5732f2d4e47ed44b44c069dc9ce68d5c424b
TalhaMehmood-web <talhamehmood991@gmail.com>
2024-10-09 16:24:33 +0500

    decresed image size

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 /* eslint-disable react/prop-types */
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-import React, { useEffect, Suspense } from "react";
+import React, { Suspense } from "react";
 import { Toaster } from "@/components/ui/sonner";
-import { useQuery } from "react-query";
-import { fetchData } from "./api/fetchData";
+
 import { useGlobalContext } from "./context/UserContext";
 import Loading from "./components/miscellaneous/loading/Loading";
+
 import ProfileAnalytics from "./pages/main/client/ProfileAnalytics";
-const UploadPictures = React.lazy(() =>
-  import("./pages/main/admin/UploadPictures")
-);
 const NewsFeed = React.lazy(() => import("./pages/main/client/NewsFeed"));
 const Home = React.lazy(() => import("./pages/main/client/Home"));
 const Portfolio = React.lazy(() => import("./pages/main/client/Portfolio"));
 const About = React.lazy(() => import("./pages/main/client/About"));
-const Orders = React.lazy(() => import("./pages/main/admin/Orders"));
+const Auction = React.lazy(() => import("./pages/main/client/Auction"));
+const Client = React.lazy(() => import("./pages/main/client/Client"));
+const Auth = React.lazy(() => import("./pages/auth/Auth"));
+// admin routes
+const Admin = React.lazy(() => import("./pages/main/admin/Admin"));
+const Pictures = React.lazy(() => import("./pages/main/admin/Pictures"));
 const Categories = React.lazy(() => import("./pages/main/admin/Categories"));
 const PictureDetails = React.lazy(() =>
   import("./pages/main/admin/PictureDetails")
 );
+const Orders = React.lazy(() => import("./pages/main/admin/Orders"));
+const UploadPictures = React.lazy(() =>
+  import("./pages/main/admin/UploadPictures")
+);
 
-const Auth = React.lazy(() => import("./pages/auth/Auth"));
-const Client = React.lazy(() => import("./pages/main/client/Client"));
-const Admin = React.lazy(() => import("./pages/main/admin/Admin"));
-const Pictures = React.lazy(() => import("./pages/main/admin/Pictures"));
-const Auction = React.lazy(() => import("./pages/main/client/Auction"));
 const PrivateRoute = ({ children }) => {
   const { user } = useGlobalContext();
   const isAuthenticated = !!user;
+
   return isAuthenticated ? children : <Navigate to={"/"} />;
 };
 
@@ -42,26 +44,6 @@ const AuthRoute = ({ children }) => {
   );
 };
 function App() {
-  const { user, setUser } = useGlobalContext();
-
-  const { data, isLoading } = useQuery(
-    "get-token",
-    () => fetchData("user/get-token"),
-    {
-      enabled: !user,
-      refetchOnMount: false,
-      staleTime: Infinity,
-    }
-  );
-  useEffect(() => {
-    setUser(data);
-    sessionStorage.setItem("user", JSON.stringify(data));
-  }, [setUser, data]);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <>
       <Routes>
--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -2,6 +2,7 @@ import axios from "../utils/AxiosConfig";
 export const fetchData = async (endpoint) => {
     try {
         const response = await axios.get(endpoint);
+
         return response.data;
     } catch (error) {
 
